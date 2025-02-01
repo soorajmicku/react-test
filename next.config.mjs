cad24687f67505d47c3c9199d39411dc48c56574
soorajmicku <soorajmickub@gmail.com>
2025-02-01 11:19:58 +0000

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push(({ context, request }, callback) => {
+        if (request && request.endsWith(".test.ts")) {
+          return callback(null, "commonjs " + request);
+        }
+        callback();
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
